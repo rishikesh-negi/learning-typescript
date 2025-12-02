6aedb9f095cbeee187421278b6744d8f7b1d6db5
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-02 19:32:06 +0530

    Add info about type alias with type inference

--- chapter-2-functions/05-type-alias.ts
+++ chapter-2-functions/05-type-alias.ts
@@ -1,9 +1,23 @@
 // In TS, certain types are way too long to be written multiple times. For instance, a function type that has a return value of function type and has several parameters, one of which accepts a function value will be extremely complicated to type in TS. To simplify this messy typing, TS provides type aliases. A type alias is a type that is some other, already defined type, but with a new name. They are similar to reusable variables, in principle. If a variable x has the value of 2 + 2, we can use x wherever we need the value 2 + 2.
 // So, type aliases are like variable that store type information instead of a specific value.
 // Creating and using a type alias:
-type LoggerCallback = (s1: string, s2: string) => string;
+export type LoggerCallback = (s1: string, s2: string) => string;
 
 export function setLoggerTimeout(LoggerCallback: LoggerCallback) {
   // do something
 }
 // Here, the setLoggerTimeout function expects a callback function as an argument. By defining the type and structure of the callback function as the LoggerCallback type alias, the function type can be reused using the LoggerCallback alias wherever needed.
+
+// Creating a type alias and letting TS infer the return type for functions conforming to that type alias:
+export type SupportResponse = (name: string) => string;
+
+// These function conform to the SupportResponse type and allow TS to infers their return type:
+export function greetCustomer(name: string) {
+  return `Hello ${name}. Welcome to Support.ai!`;
+}
+
+export function farewellCustomer(name: string) {
+  return `Goodbye ${name}. See you soon...`;
+}
+
+// Type aliases are scoped only to TS and cannot be referenced for JS oeprations like variable comparison. The SupportResponse type alias, for example, cannot be used in program logic in JS.
