f2e975e434b31be84c0b624ec1df8045957ff321
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-07 19:48:08 +0530

    File about optional object properties in TS object types

---
+++ ch5-objects/03-optional-object.properties.ts
@@ -0,0 +1,25 @@
+// Optional Object Properties:
+// This TS feature is incredibly useful and is used very often. Just like a function can have optional parameters, objects can have optional properties. Optional properties can be added to an object type with the '?' operator:
+type Shinigami = {
+  name: string;
+  reiatsu: number;
+  bankai?: string;
+};
+// This makes it so that the type of "bankai" is (string | undefined), just like optional parameters.
+
+type Mail = {
+  from: string;
+  to: string[];
+  subject: string;
+  body: string;
+  urgent: boolean;
+  cc?: string[];
+};
+
+function processMail(mail: Mail) {
+  return `FROM: ${mail.from}
+  TO: ${mail.to.join(", ")}
+  ${mail.cc ? mail.cc.join(", ") : ""}
+  SUBJECT: ${mail.subject}
+  BODY: ${mail.body}`;
+}
