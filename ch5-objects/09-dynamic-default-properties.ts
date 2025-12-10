126bdb29abe241ac869eb7381c4f4845f9bf5480
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-10 18:40:57 +0530

    Fix concept explanation

--- ch5-objects/09-dynamic-default-properties.ts
+++ ch5-objects/09-dynamic-default-properties.ts
@@ -1,12 +1,12 @@
 // Dynamic Default Properties:
-// Sometimes, we need an object type where some properties are required, and the remaining properties are dynamic. Ex:
+// Sometimes, we need an object type where some properties are required, and the remaining properties are dynamic (dynamic keys). Ex:
 type FormData = {
   [field: string]: string;
   email: string;
   password: string;
 };
-// Here, "email" and "password" are the mandatory fields, and the object can have other dynamic (and optional) properties with string-type key and value.
-// Dynamic properties should not be overused, especially with mandatory properties. Usually we should opt for a fully specific object or a fully dynamic one. There are very few niche use cases where dynamic and mandatory properties need to be used together in the same object.
+// Here, "email" and "password" are the mandatory fields, and the object can have other dynamic (and optional) properties with string-type keys and values.
+// Dynamic properties should not be overused, especially in combination with mandatory properties. Usually we should opt for a fully specific object or a fully dynamic one. There are very few niche use cases where dynamic and mandatory properties need to be used together in the same object.
 
 type MailPreferences = {
   doNotDisturb: boolean;
