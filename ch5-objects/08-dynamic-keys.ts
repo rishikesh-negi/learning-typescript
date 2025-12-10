b7c385d8b93cc4349a825fc736c5bfae61828433
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-10 18:38:27 +0530

    Fix concept explanation

--- ch5-objects/08-dynamic-keys.ts
+++ ch5-objects/08-dynamic-keys.ts
@@ -3,7 +3,7 @@
 type UserMetrics = {
   [key: string]: number;
 };
-// The above type means that any UserMetric type object can have any number of properties if the keys are strings and the value of each is a number. The "key" identifier in the above type is used only to name the variable. It could be anything, and it doesn't influence the names of the actual keys the objects of UserMetrics type. It's common to just call that variable "key" when typing dynamic keys.
+// The above type means that any UserMetric type object can have any number of properties if the keys are strings and each value is a number. The "key" identifier in the above type is used only to name the variable in the type definition. It could be anything, and it doesn't influence the names of the actual keys the objects of UserMetrics type. It's common to just call that variable "key" when typing dynamic keys.
 
 type MailPreferences = {
   [key: string]: boolean;
@@ -20,7 +20,7 @@ function setPreference(
   return exists;
 }
 
-// There's another way to create an object with dynamic keys using Records in TS. A Record is just an object in TS. We can specify the types of key and value in a Record:
+// There's another way to create an object with dynamic keys using Records in TS. A Record is just an object in TS. We can specify the types of keys and values in a Record:
 type MailPreferences2 = Record<string, boolean>;
-// In the above type, Record<string, boolean> simply means that values of MailPreferences2 type are objects with keys being string and values being boolean.
+// In the above type, Record<string, boolean> simply means that values of MailPreferences2 type are objects with the keys being strings and values being boolean.
 // MailPreferences and MailPreferences2 are exactly the same for all purposes
