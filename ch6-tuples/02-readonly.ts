b0b00362587f88611b2d214102e973d0a0b7a2e9
Rishikesh Negi <rishinegi97@gmail.com>
2025-12-22 18:45:43 +0530

    Fix existing explanation and add more explanatory comments

--- ch6-tuples/02-readonly.ts
+++ ch6-tuples/02-readonly.ts
@@ -1,13 +1,13 @@
 // Readonly:
 // TS Tuples are still arrays under the hood. So, we can still push to them and pop from them, which are invalid tuple operations in other languages with real tuples, because real tuples have a fixed length:
 const nameAndAge: [string, number] = ["John Doe", 30];
-nameAndAge.push("Joe Joseph"); // Should not be allowed
+nameAndAge.push("Joe Joseph"); // Should not be allowed, but works
 
 // Fortunately, TS allows "readonly" tuples:
 const nameAndAgeReadonly: readonly [string, number] = ["John Johnson", 30];
 // nameAndAgeReadonly.push("Hello world"); // Error: Property 'push' does not exist on type 'readonly [string, number]'
 
-// However, making the tuple readonly makes the elements read-only. So, we cannot change the value of any of the elements even if we use the correct type:
+// However, making the tuple readonly also makes its elements read-only. So, we cannot change the value of any of the elements even if we use the correct type:
 // nameAndAgeReadonly[0] = "Hello Worlds"; // Cannot assign to '0' because it is a read-only property
 
 // Conclusion: If a strictly immutable tuple with specific values is needed, make it read-only. If the elements' values need to change, use regular tuples and avoid adding or removing elements to or from it.
@@ -20,5 +20,6 @@ function resolveTicket(ticket: Ticket) {
   // Pitfall of readonly tuple type:
   // return [ticket[0], ticket[1], ticket[2], true]; // Error: Type '(string | number | boolean)[]' is not assignable to type 'Ticket'
 
+  // The above error got raised because we're assigning a regular, mutable array to the Ticket return type, which is readonly. So, we have to return an "as const" array:
   return [ticket[0], ticket[1], ticket[2], true] as const;
 }
